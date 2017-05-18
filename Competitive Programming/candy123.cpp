474ffec644289f1fc65d5d27ad7eea795e531665
Drishti Agarwal <say2drishti@gmail.com>
2017-05-18 08:46:48 +0530

    Bear And Candy

--- Competitive Programming/candy123.cpp
+++ Competitive Programming/candy123.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t,a,b,x,y;
+cin>>t;
+while(t--)
+{
+cin>>a>>b;
+x=1;y=2;
+while(a>=0 && b>=0)
+{
+a-=x;x+=2;b-=y;y+=2;
+//cout<<a<<" "<<x<<" "<<b<<" "<<y<<endl;
+}
+if(a<0)
+cout<<"Bob"<<endl;
+else
+cout<<"Limak"<<endl;
+}
+return 0;
+}
 
