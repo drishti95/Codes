9a7b513c5fc31e9d61f2e320ccc8cea03ad343ed
Drishti Agarwal <say2drishti@gmail.com>
2017-05-28 11:53:27 +0530

    SnackDown preleimination A

---
+++ Competitive Programming/teamform.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t,x,y,a,b,p;
+cin>>t;
+
+
+while(t--)
+{
+
+cin>>x>>y;
+p=y;
+while(p--)
+cin>>a>>b;
+if(x%2)
+cout<<"no"<<endl;
+else
+cout<<"yes"<<endl;
+}
+return 0;
+}
