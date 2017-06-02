4a0ce62a333a8117d3e613e1bd8a3b20be389a16
Drishti Agarwal <say2drishti@gmail.com>
2017-06-02 21:28:49 +0530

    updated code

--- Competitive Programming/sumq.cpp
+++ Competitive Programming/sumq.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-	long long int t,a[100000],b[100000],c[100000],p,q,r,i,j,k,sum;
+	long long int t,a[100000],b[100000],c[100000],p,q,r,i,j,k,sum,a1,a3;
 	cin>>t;
 	while(t--)
 	{
@@ -14,21 +14,23 @@ int main() {
 		for(k=0;k<r;k++)
 		cin>>c[k];
 		sum=0;
-		for(i=0;i<p;i++)
-		{
-			for(j=0;j<q;j++)
-			{
-				if(a[i]>b[j])
-				continue;
-				for(k=0;k<r;k++)
-				{
-					if(b[j]<c[k])
-					continue;
-					sum=(sum + ((a[i]+b[j])%1000000007*(b[j]+c[k])%1000000007)%1000000007)%1000000007;
-					sum=sum%1000000007;
-				}
-			}
-		}
+        for(j=0;j<q;j++)
+        {
+            a1=0;
+            for(i=0;i<p;i++)
+            {
+            if(a[i]<=b[j])
+            a1=(a1+(a[i]+b[j])%1000000007)%1000000007;
+            }
+            a3=0;
+            for(k=0;k<r;k++)
+            {
+                if(b[j]>=c[k])
+                a3=(a3+(c[k]+b[j])%1000000007)%1000000007;
+            }
+            sum=(sum + (a1*a3)%1000000007)%1000000007;
+
+        }
 		cout<<sum<<endl;
 	}
 
